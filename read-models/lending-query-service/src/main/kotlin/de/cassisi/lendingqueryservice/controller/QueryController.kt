1d340a50887a8004e8715a6f044fa54a2ac4dc48
Domenic Cassisi <domenic.cassisi@web.de>
2022-07-28 10:41:10 +0200

    Lending Query Service initial commit

---
+++ read-models/lending-query-service/src/main/kotlin/de/cassisi/lendingqueryservice/controller/QueryController.kt
@@ -0,0 +1,23 @@
+package de.cassisi.lendingqueryservice.controller
+
+import de.cassisi.lendingqueryservice.model.BookDocument
+import de.cassisi.lendingqueryservice.service.QueryService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+@RestController
+class QueryController(private val queryService: QueryService) {
+
+    @GetMapping("/all")
+    fun getAll(): List<BookDocument> {
+        return queryService.getAll().toList()
+    }
+
+    @GetMapping
+    fun getStudent(@RequestParam studentId: UUID): BookDocument {
+        return queryService.getById(studentId.toString())
+    }
+
+}
---
+++ read-models/lending-query-service/src/main/kotlin/de/cassisi/lendingqueryservice/service/QueryService.kt
@@ -0,0 +1,18 @@
+package de.cassisi.lendingqueryservice.service
+
+import de.cassisi.lendingqueryservice.model.BookDocument
+import de.cassisi.lendingqueryservice.model.BookRepository
+import org.springframework.stereotype.Service
+
+@Service
+class QueryService(private val bookRepository: BookRepository) {
+
+    fun getAll(): MutableList<BookDocument> {
+        return bookRepository.findAll()
+    }
+
+    fun getById(studentId: String): BookDocument {
+        return bookRepository.findById(studentId).get()
+    }
+
+}
